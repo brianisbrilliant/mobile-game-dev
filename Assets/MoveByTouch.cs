6fba3c617bc246cd342d39e5c152d30b7a1773fd
Brian Foster <brianfoster@Brians-MacBook-Pro.local>
2018-12-18 17:07:08 -0600

    adding touch functionality

---
+++ Assets/MoveByTouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+	public int touchFunctionSelector = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(touchFunctionSelector == 0) {
+			if(Input.touchCount > 0) {
+				Touch touch = Input.GetTouch(0);
+				Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+				touchPos.z = 0f;
+				this.transform.position = touchPos;
+
+			}
+		}
+		if(touchFunctionSelector == 1) {
+			for(int i = 0; i < Input.touchCount; i++) {
+				Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+				Debug.DrawLine(Vector3.zero, touchPos, Color.red);
+			}
+		}
+	}
+
+}
--- Assets/Scripts/SpinCube.cs
+++ Assets/Scripts/SpinCube.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SpinCube : MonoBehaviour
 {
+    private bool isHalfSpeed = false;
 
     private Quaternion spinDirection;
+
+    public ParticleSystem touchExplosion;
+	public Text debugText;
+
+
+    private Vector3 position;
+    private float width;
+    private float height;
 
     // Start is called before the first frame update
     void Start()
     {
         spinDirection = Random.rotation;
+
+        // center of the screen
+        width = (float)Screen.width;
+        height = (float)Screen.height;
+        position = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -18,8 +31,69 @@ public class SpinCube : MonoBehaviour
     {
         transform.Rotate(spinDirection.eulerAngles * Time.deltaTime);
 
-        if(Input.anyKeyDown) {
-            spinDirection = Random.rotation;
-        }
+		if(Input.touchCount > 0) {
+			// starting with the first touch
+			Touch touch = Input.GetTouch(0);
+
+			if(touch.phase == TouchPhase.Began) {
+				// calculate screen position
+				Vector3 newPos = new Vector3((touch.position.x / width * 12) - 2, (touch.position.y / height) * 10, 0);
+				debugText.text = "touchpos = " + newPos.ToString("0.00");
+				// create new particle explosion at the position of the touch in screeen space
+				ParticleSystem ps =  Instantiate(touchExplosion, newPos, Quaternion.identity);
+				GameObject cubeMarker = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				cubeMarker.transform.position = newPos;
+				cubeMarker.transform.localScale = Vector3.one * 0.5f;
+				// destroy the particle system after the duration of the particle system
+				Destroy(ps.gameObject, touchExplosion.main.duration);
+			}
+		}
+
+		// Unity's implementation of touch. useful example.
+		if(false) {
+			// Handle screen touches.
+			if (Input.touchCount > 0)
+			{
+				Touch touch = Input.GetTouch(0);
+
+				// Move the cube if the screen has the finger moving.
+				if (touch.phase == TouchPhase.Moved)
+				{
+					Vector2 pos = touch.position;
+					pos.x = (pos.x - width) / width;
+					pos.y = (pos.y - height) / height;
+					position = new Vector3(-pos.x, pos.y, 0.0f);
+
+					// Position the cube.
+					transform.position = position;
+				}
+
+				if (Input.touchCount == 2)
+				{
+					touch = Input.GetTouch(1);
+
+					if (touch.phase == TouchPhase.Began)
+					{
+						// Halve the size of the cube.
+						transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+					}
+
+					if (touch.phase == TouchPhase.Ended)
+					{
+						// Restore the regular size of the cube.
+						transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+					}
+				}
+			}
+		}
+
     }
+
+    public void HalfSpeed() {
+        if(isHalfSpeed) Time.timeScale = 1;
+        else Time.timeScale = 0.5f;
+        isHalfSpeed = !isHalfSpeed;
+    }
+
+	public void NewSpin() { spinDirection = Random.rotation; }
 }
