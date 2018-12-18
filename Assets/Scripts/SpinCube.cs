42650ede89993711e48c24368f6c6459c1ba4192
Brian Foster <brianfoster@Brians-MacBook-Pro.local>
2018-12-18 13:55:20 -0600

    initial commit, starting spincube scene.

---
+++ Assets/Scripts/SpinCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCube : MonoBehaviour
+{
+
+    private Quaternion spinDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spinDirection = Random.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(spinDirection.eulerAngles * Time.deltaTime);
+
+        if(Input.anyKeyDown) {
+            spinDirection = Random.rotation;
+        }
+    }
+}
